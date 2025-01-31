b19c9957af621380004f85d6c04fab0e54d8ad77
Anishz77 <pantaanish77@gmail.com>
2025-01-31 11:46:04 +0545

    updated

--- src/components/BuyNow.jsx
+++ src/components/BuyNow.jsx
@@ -73,7 +73,7 @@ const BuyNow = () => {
           </div>
           <button className="buy-button" onClick={addToCart}>Buy Now</button>
           <div className="additional-info">
-            <div>Free D</div>
+            <div>Free De</div>
             <div>Free 30 Days Delivery Returns. Details</div>
           </div>
         </div>
