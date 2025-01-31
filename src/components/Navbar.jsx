51421f7a0c3ff87375870676daabf2aa56ef7fdf
Anishz77 <pantaanish77@gmail.com>
2025-01-31 11:45:09 +0545

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -92,7 +92,7 @@ const Navbar = () => {
                         value={searchQuery}
                         onChange={handleSearchChange}
                     />
-                    <button className="btn btn-outline-success" type="submit">Sea</button>
+                    <button className="btn btn-outline-success" type="submit">Sear</button>
                 </form>
             </ul>
             <div className="nav-login-cart">
